e8744ba7f3e7bdab8fbbccf023288f14093f535f
Soufiane <safilo2016@gmail.com>
2024-01-16 18:27:01 +0100

    debug

--- app.py
+++ app.py
@@ -220,5 +220,5 @@ def go_back(source):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(debug=False, host="0.0.0.0")
+    # app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0")
